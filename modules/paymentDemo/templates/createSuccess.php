93a95e6fc4cc33c14dcc2265b7c7ab3f29a069b0
Johannes M. Schmitt <schmittjoh@gmail.com>
2010-09-16 16:12:28 +0200

    added some JS to improve user experience

--- modules/paymentDemo/templates/createSuccess.php
+++ modules/paymentDemo/templates/createSuccess.php
@@ -1,5 +1,25 @@
 <?php use_helper('Url') ?>
 
+<script language="javascript" type="text/javascript">
+  $(function() {
+    $('#paymentDemoForm_method').change(function() {
+      var selected = $(this).children(':selected').get(0).value;
+
+      $(this).parent().parent().siblings('tr:contains("Method")').each(function() {
+        if ($(this).is(':contains("'+selected+'")'))
+        {
+        	  $(this).show();
+        }
+        else
+        {
+        	  $(this).hide();
+        }
+      });
+      
+    }).trigger('change');
+  });
+</script>
+
 <h2>Create New Payment</h2>
 
 <?php echo $form->renderFormTag(url_for('paymentDemo/create')) ?>
